4a9e85c4fc7204078865c093728ccb367cc60fbb
hkhailee <42651565+hkhailee@users.noreply.github.com>
2020-05-03 12:09:13 -0600

    changes for andrew

--- Game-master/make.sh
+++ Game-master/make.sh
@@ -2,9 +2,12 @@
 pwd
 cd src/
 pwd
+javac game/animation/ease/*.java
+javac game/animation/tween/*.java
 javac game/debug/*.java
 javac game/framework/*.java
 javac game/graphics/*.java
+javac game/gui/*.java
 javac game/input/*.java
 
 jar cf Game.jar .
